378da1191447f433b4db21726d168a0ba71ee222
Augusto <augustocristian@uniovi.es>
2023-11-16 15:49:37 +0100

    Migrating to Selema, siplifying all

--- src/test/java/giis/eshopcontainers/e2e/functional/common/BaseLoggedClass.java
+++ src/test/java/giis/eshopcontainers/e2e/functional/common/BaseLoggedClass.java
@@ -2,14 +2,19 @@ package giis.eshopcontainers.e2e.functional.common;
 
 import giis.eshopcontainers.e2e.functional.utils.Click;
 import giis.eshopcontainers.e2e.functional.utils.Navigation;
+import giis.eshopcontainers.e2e.functional.utils.Waiter;
+import giis.selema.framework.junit5.LifecycleJunit5;
+import giis.selema.manager.SeleManager;
+import giis.selema.services.impl.SelenoidService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.logging.LogEntries;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,10 +23,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Date;
 import java.util.Properties;
-
-import static org.openqa.selenium.logging.LogType.BROWSER;
 
 /*
  * This class contains common set-up, tear-down, browser setup, login, and logout methods utilized across various
@@ -29,13 +31,20 @@ import static org.openqa.selenium.logging.LogType.BROWSER;
  * consistent set-up and tear-down procedures before each case. Additionally, it provides common clearance and
  * preparation methods shared among the test cases.
  */
+@ExtendWith(LifecycleJunit5.class)
 public class BaseLoggedClass {
 
     public static final Logger log = LoggerFactory.getLogger(BaseLoggedClass.class);
     protected static String sutUrl;
     protected static String tjobName = "DEFAULT_TJOB";
     protected static Properties properties;
-    protected BrowserUser user;
+    protected WebDriver driver;
+    protected Waiter waiter;
+    private static SeleManager seleManager = new SeleManager();
+    private String userName;
+    private String password;
+
+    private boolean isLogged = false;
 
     @BeforeAll()
     static void setupAll() throws IOException { //28 lines
@@ -52,87 +61,90 @@ public class BaseLoggedClass {
             log.debug("Configuring the browser to connect to the remote System Under Test (SUT) at the following URL: " + sutUrl);
         }
         log.info("Ending global setup for all test cases.");
+        setupBrowser();
+
     }
 
     @BeforeEach
-    void setup(TestInfo testInfo) throws MalformedURLException { //65 lines
+    void setup(TestInfo testInfo) { //65 lines
+        driver = seleManager.getDriver();
+        this.waiter = new Waiter(driver);
+        driver.get(sutUrl);
         log.info("Starting Individual Set-up for the test:" + testInfo.getDisplayName() + ".");
         tjobName = System.getProperty("tjob_name");
-        user = setupBrowser(testInfo.getDisplayName(), 1);
         log.info("Individual Set-up finished, starting test" + testInfo.getDisplayName() + "...");
+        userName = properties.getProperty("USER_ESHOP");
+        password = properties.getProperty("USER_ESHOP_PASSWORD");
+
     }
 
-    protected BrowserUser setupBrowser(String testName,
-                                       int secondsOfWait) throws MalformedURLException {
+    protected static void setupBrowser() {
         log.debug("Starting browser ({})", properties.getProperty("BROWSER_USER"));
-        BrowserUser browserUser = new ChromeUser(secondsOfWait, testName, properties.getProperty("USER_ESHOP"),properties.getProperty("USER_ESHOP_PASSWORD") );
+
+        if (System.getenv("SELENOID_PRESENT") == null) {
+            log.info("Configuring Chrome WebDriver (Local)");
+            seleManager.setBrowser("chrome").setArguments(new String[]{"--start-maximized"});
+        } else {
+            seleManager.setDriverUrl("http://selenoid:4444/wd/hub").add(new SelenoidService().setVideo().setVnc());
+
+        }
         log.debug("Navigating to {}", sutUrl);
-        browserUser.getDriver().get(sutUrl);
-        return browserUser;
+
+
     }
 
     /**
      * Navigates to the main menu to enter a user session.
-     * @param user The user with its remote Web Driver.
      */
-    protected void login(BrowserUser user) throws ElementNotFoundException {
-        Navigation.toMainMenu(user);
+    protected void login() throws ElementNotFoundException {
+        Navigation.toMainMenu(driver, waiter);
 
-        user.setIsLogged(true);
-        log.debug("Logging in user {} ", user.getUserId());
-        user.waitUntil(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Login')]")), "The button searched by xPath //a[contains(text(),'Login')] is not clickable");
-        Click.element(user, user.getDriver().findElement(By.xpath("//a[contains(text(),'Login')]")));
+        isLogged = true;
+        log.debug("Logging in user {} ", userName);
+        waiter.waitUntil(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(text(),'Login')]")), "The button searched by xPath //a[contains(text(),'Login')] is not clickable");
+        Click.element(driver, waiter, driver.findElement(By.xpath("//a[contains(text(),'Login')]")));
 
-        user.waitUntil(ExpectedConditions.presenceOfElementLocated(By.id("Username")), "The Username login field is not present");
-        WebElement userNameField = user.getDriver().findElement(By.id("Username"));
-        user.waitUntil(ExpectedConditions.presenceOfElementLocated(By.id("Password")), "The Password login is not present");
-        WebElement userPassField = user.getDriver().findElement(By.id("Password"));
-        userNameField.sendKeys(user.getUserId());
-        userPassField.sendKeys(user.getPassword());
+        waiter.waitUntil(ExpectedConditions.presenceOfElementLocated(By.id("Username")), "The Username login field is not present");
+        WebElement userNameField = driver.findElement(By.id("Username"));
+        waiter.waitUntil(ExpectedConditions.presenceOfElementLocated(By.id("Password")), "The Password login is not present");
+        WebElement userPassField = driver.findElement(By.id("Password"));
+        userNameField.sendKeys(userName);
+        userPassField.sendKeys(password);
 
-        user.waitUntil(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(.,'Login')]")), "The button searched by xpath //button[contains(.,'Login')] is not clickable");
-        Click.element(user, user.getDriver().findElement(By.xpath("//button[contains(.,'Login')]")));
-        log.debug("Logging in successful for user {}", user.getUserId());
+        waiter.waitUntil(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(.,'Login')]")), "The button searched by xpath //button[contains(.,'Login')] is not clickable");
+        Click.element(driver, waiter, driver.findElement(By.xpath("//button[contains(.,'Login')]")));
+        log.debug("Logging in successful for user {}", userName);
     }
 
     /**
      * Navigates to the main menu and log-outs the user in session
      */
-    protected void logout(BrowserUser user) throws ElementNotFoundException { //43 lines
-        Navigation.toMainMenu(user);
+    protected void logout() throws ElementNotFoundException { //43 lines
+        Navigation.toMainMenu(driver, waiter);
         WebElement elementOfInterest;
         try {
             //trying to get the "select option"
-            elementOfInterest = user.getDriver().findElement(By.xpath("//*[@id=\"logoutForm\"]/section[2]/a[2]/div"));
+            elementOfInterest = driver.findElement(By.xpath("//*[@id=\"logoutForm\"]/section[2]/a[2]/div"));
         } catch (NoSuchElementException e) {
             //so options are not visible, meaning we need to click first
-            WebElement mainMenu = user.getDriver().findElement(By.className("esh-identity-drop"));
-            Click.element(user, mainMenu);
+            WebElement mainMenu = driver.findElement(By.className("esh-identity-drop"));
+            Click.element(driver, waiter, mainMenu);
             //good idea would be to put "wait for element" here
             elementOfInterest = mainMenu.findElement(By.xpath("//*[@id=\"logoutForm\"]/section[2]/a[2]/div"));
         }
         //this would select the option
-        Click.element(user, elementOfInterest);
-        user.setIsLogged(false);
+        Click.element(driver, waiter, elementOfInterest);
+        isLogged = false;
         log.debug("Logging out");
     }
 
     @AfterEach
     void tearDown(TestInfo testInfo) throws ElementNotFoundException { //13 lines
-        if (user != null) {
-            log.info("Finish test: {} - Driver {}", testInfo.getDisplayName(), this.user.getDriver());
-            log.debug("Browser console at the end of the test");
-            LogEntries logEntries = user.getDriver().manage().logs().get(BROWSER);
-            logEntries.forEach((entry) -> log.info("[{}] {} {}",
-                    new Date(entry.getTimestamp()), entry.getLevel(),
-                    entry.getMessage()));
-            if (user.isLogged()) {
-                this.logout(user);
-            }
-            log.info("Disposing user and releasing/closing browser");
-            user.dispose();
+        if (isLogged) {
+            this.logout();
         }
-
+        log.info("Disposing user and releasing/closing browser for the test" + testInfo.getDisplayName());
     }
 
 }
+
--- src/test/java/giis/eshopcontainers/e2e/functional/utils/Navigation.java
+++ src/test/java/giis/eshopcontainers/e2e/functional/utils/Navigation.java
@@ -1,18 +1,19 @@
 package giis.eshopcontainers.e2e.functional.utils;
 
-import giis.eshopcontainers.e2e.functional.common.BrowserUser;
 import giis.eshopcontainers.e2e.functional.common.ElementNotFoundException;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 /**
  * Class with various methods facilitating navigation within the web UI of the application: return to the main page
  */
 public class Navigation {
     /**
      * Click on the top logo to return to the main menu.
-     * @param user Browser user with the remote web driver to perform the action
+     * @param driver remote web driver to perform the action
      */
-    public static BrowserUser toMainMenu(BrowserUser user) throws ElementNotFoundException {
-        Click.element(user, user.getDriver().findElement(By.xpath("//img")));
-        return user;
+    public static void toMainMenu(WebDriver driver, Waiter waiter) throws ElementNotFoundException {
+        Click.element(driver,waiter, driver.findElement(By.xpath("//img")));
+
     }
 }
