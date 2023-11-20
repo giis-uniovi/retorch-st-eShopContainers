edf3dbef139c1bc872bf563aa64ad485a56afc56
Augusto <augustocristian@uniovi.es>
2023-11-21 00:49:44 +0100

    Using docker addressing to interconect containers - Removing unused versions and comments poms - Simplifying set-up browser and logs - Declared locally the variable SUT_URL removed from prop. file

--- retorchfiles/scripts/coilifecycles/coi-teardown.sh
+++ retorchfiles/scripts/coilifecycles/coi-teardown.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 COITEARDOWNSTART="$(date +%s%3N)"
-
-echo 'Switch off all containers...'
-docker stop "$(docker ps | grep eShopContainers | awk '{print \$1}')" || echo 'All the containers are stopped!'
-docker rm --volumes "$(docker ps -a | grep eShopContainers | awk '{print \$1}')" || echo 'All the containers are removed!'
 cd "$SUT_LOCATION/src"
 docker compose -f docker-compose.yml -f docker-compose.retorch.yml down
+echo 'Removing all the volumes not used'
+docker volume prune --all -f
 cd $WORKSPACE
 
 $E2ESUITE_URL/retorchfiles/scripts/saveTJobLifecycledata.sh
@@ -14,4 +12,3 @@ COITEARDOWNEND="$(date +%s%3N)"
 
 OUTPUTDIRCOI="$WORKSPACE/retorchcostestimationdata/exec$BUILD_NUMBER/COI.data"
 echo -n ";$COITEARDOWNSTART;$COITEARDOWNEND" >>"$OUTPUTDIRCOI"
-
