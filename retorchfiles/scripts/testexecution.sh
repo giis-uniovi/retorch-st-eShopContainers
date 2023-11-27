1fde22447622b6d4a60b86e4327f258ef45b1383
Augusto <augustocristian@uniovi.es>
2023-11-27 16:46:13 +0100

    Undo the compose  and using internal networking

--- retorchfiles/scripts/testexecution.sh
+++ retorchfiles/scripts/testexecution.sh
@@ -2,5 +2,8 @@
 #https://stackoverflow.com/questions/4365418/get-state-of-maven-execution-within-shell-script
 TJOB_NAME="$1"
 $E2ESUITE_URL/retorchfiles/scripts/writetime.sh "$2" "$1" #$2 is the stage, $1 tjob
-mvn test -Ddirtarget=TJOBExample -Dtjob_name=tjobe -DSUT_URL=webmvc-$TJOB_NAME
+export DOCKER_HOST_IP=$(/sbin/ip route|awk '/default/ { print $3 }')
+echo "The HOST_IP is: $DOCKER_HOST_IP"
+
+mvn test -Ddirtarget=TJOBExample -Dtjob_name=tjobe -DSUT_URL=$DOCKER_HOST_IP:5100
 $E2ESUITE_URL/retorchfiles/scripts/writetime.sh "$2" "$1" #$2 is the stage, $1 tjob
