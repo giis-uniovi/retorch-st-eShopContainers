6974f99589d8a38c1d080570263fc10671a2a172
Augusto <augustocristian@uniovi.es>
2023-11-28 22:11:00 +0100

    Adding logs and removing verbose javadoc comments

--- src/test/java/giis/eshopcontainers/e2e/functional/CatalogTests.java
+++ src/test/java/giis/eshopcontainers/e2e/functional/CatalogTests.java
@@ -11,27 +11,30 @@ import org.openqa.selenium.WebElement;
 
 import static giis.eshopcontainers.e2e.functional.utils.Shopping.*;
 
-class ShoppingBasketTests extends BaseLoggedClass {
+class CatalogTests extends BaseLoggedClass {
     @Test
     @DisplayName("AddProductsToBasket")
     void addProductsToBasket() throws ElementNotFoundException {
+        log.debug("Checking that the product buttons are disabled before the user logs-in");
         WebElement productCupButton = driver.findElement(By.xpath("/html/body/div/div[3]/div[1]/form/input[1]"));
         Assertions.assertEquals("esh-catalog-button is-disabled", productCupButton.getAttribute("class"), "Before Login the eShop product button was expected for being disabled and was enabled");
         this.login();
         WebElement loggedUser = driver.findElement(By.xpath("//*[@id=\"logoutForm\"]/section[1]/div"));
         Assertions.assertEquals("alice", loggedUser.getText(), "The user logged is not the expected, the expected was alice and the obtained is " + loggedUser.getText());
+        log.debug("Adding the first element: NetCore Cup ");
         productCupButton = driver.findElement(By.xpath("/html/body/div/div[3]/div[1]/form/input[1]"));
         Assertions.assertEquals("esh-catalog-button ", productCupButton.getAttribute("class"), "The eShop product button was expected for being enabled and was disabled");
         Click.element(driver, waiter, productCupButton);
-        Assertions.assertEquals(1, getNumShoppingItems(driver, waiter));
+        Assertions.assertEquals(1, getNumShoppingItems(driver, waiter), "The number of elements in the basket doesn't match");
+        log.debug("Adding Two elements more: Hoodie and CupPin");
         WebElement productHoodieBlackButton = driver.findElement(By.xpath("/html/body/div/div[3]/div[3]/form/input[1]"));
         Click.element(driver, waiter, productHoodieBlackButton);
         Assertions.assertEquals(2, getNumShoppingItems(driver, waiter));
         WebElement productCupPinButton = driver.findElement(By.xpath("/html/body/div/div[3]/div[6]/form/input[1]"));
         Click.element(driver, waiter, productCupPinButton);
         Assertions.assertEquals(3, getNumShoppingItems(driver, waiter));
-        eraseShoppingBasket(driver, waiter);
         this.logout();
+        log.debug("Checking that the product buttons are disabled after the logout...");
         productCupButton = driver.findElement(By.xpath("/html/body/div/div[3]/div[1]/form/input[1]"));
         Assertions.assertEquals("esh-catalog-button is-disabled", productCupButton.getAttribute("class"), "After Log-Out the eShop product buttons was expected for being disabled and was enabled");
     }
@@ -51,8 +54,6 @@ class ShoppingBasketTests extends BaseLoggedClass {
                         "Brand:" + new String[]{"All Brands", "Net Core", "Others"}[numBrand - 1] + " Type:" +
                                 new String[]{"All Types", "Mug", "TShirt", "Pin"}[numType - 1] + " Num items exp:" + numItems[numBrand - 1][numType - 1]);
             }
-
         }
     }
-
 }
--- src/test/java/giis/eshopcontainers/e2e/functional/utils/Shopping.java
+++ src/test/java/giis/eshopcontainers/e2e/functional/utils/Shopping.java
@@ -9,6 +9,9 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Class with various methods facilitating the catalog-shopping actions
+ */
 public class Shopping {
     public static final Logger log = LoggerFactory.getLogger(Shopping.class);
 
@@ -19,19 +22,8 @@ public class Shopping {
         return Integer.valueOf(basketElements.getText());
     }
 
-    public static void eraseShoppingBasket(WebDriver driver, Waiter waiter) throws ElementNotFoundException {
-        Navigation.toMainMenu(driver, waiter);
-        if (getNumShoppingItems(driver, waiter) != 0) {
-            waiter.waitUntil(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("/html/body/header/div/article/section[3]/a"))), "The basket icon is not visible");
-            WebElement buttonBasket = driver.findElement(By.xpath("/html/body/header/div/article/section[3]/a"));
-            Click.element(driver, waiter, buttonBasket);
-        }
-    }
-
     /**
      * This method applies several brand filters to the eShopOnContainers catalog
-     * @param driver WebDriver with the browser
-     * @param waiter Waiter of the provided webdriver
      * @param option Type of filter: 1) All brands, 2)NETCore and 3) Others
      */
     public static void selectBrandFilter(WebDriver driver, Waiter waiter, Integer option) throws ElementNotFoundException {
@@ -46,17 +38,14 @@ public class Shopping {
             //good idea would be to put "wait for element" here
             elementOfInterest = mainMenu.findElement(By.xpath("//*[@id=\"BrandFilterApplied\"]/option[" + option + "]"));
         }
-
         log.debug("Selecting the Brand: " + new String[]{"All Brands", "Net Core", "Others"}[option - 1]);
-
         Click.element(driver, waiter, elementOfInterest);
-
     }
 
-    /*  This method applies several type filters to the eShopOnContainers catalog
-     * @param driver WebDriver with the browser
-     * @param waiter of the provided web-driver
-     * @param option Type of filter: 1) All Types, 2) Mug, 3) TShirt and 4)Pin */
+    /**
+     * This method applies several type filters to the eShopOnContainers catalog
+     * @param option Type of filter: 1) All Types, 2) Mug, 3) TShirt and 4)Pin
+     */
     public static void selectTypeFilter(WebDriver driver, Waiter waiter, Integer option) throws ElementNotFoundException {
         WebElement elementOfInterest;
         try {
@@ -69,27 +58,31 @@ public class Shopping {
             //good idea would be to put "wait for element" here
             elementOfInterest = mainMenu.findElement(By.xpath("//*[@id=\"TypesFilterApplied\"]/option[" + option + "]"));
         }
-
         log.debug("Selecting the Type: " + new String[]{"All Types", "Mug", "TShirt", "Pin"}[option - 1]);
         Click.element(driver, waiter, elementOfInterest);
-
     }
 
+    /**
+     * Get the number of displayed items in the catalog, counting also the items in the second page (if exist)
+     */
     public static Integer numberCatalogDisplayedItems(WebDriver driver, Waiter waiter) throws ElementNotFoundException {
         int numelements = 0;
+        log.debug("Getting the next button to check its visibility");
         WebElement nextButton = driver.findElement(By.id("Next"));
         if (nextButton.isDisplayed()) {
             numelements += driver.findElements(By.className("esh-catalog-item")).size();
-            log.debug("Clicking to go to the next page");
+            log.debug("Clicking to go to the next page, the first page number of items is:" + numelements);
             Click.element(driver, waiter, nextButton);
         } else {
             log.debug("Button not visible, counting only the elements of the webpage");
         }
         java.util.List<WebElement> childDivsWithClass = driver.findElements(By.className("esh-catalog-item"));
+        log.debug("The total number of items is:" + numelements + childDivsWithClass.size());
         return numelements + childDivsWithClass.size();
     }
 
     public static void clickApplyFilterButton(WebDriver driver, Waiter waiter) throws ElementNotFoundException {
+        log.debug("Click the Filter Apply button");
         WebElement filterApplyButton = driver.findElement(By.xpath("/html/body/section[2]/div/form/input[1]"));
         Click.element(driver, waiter, filterApplyButton);
     }
