41b4a45fd7bbeba0e4ae80eed3d9f9a4929bf97c
Augusto <augustocristian@uniovi.es>
2023-12-04 00:09:45 +0100

    Adding the correct env. file

--- src/test/java/giis/eshopcontainers/e2e/functional/OrderTests.java
+++ src/test/java/giis/eshopcontainers/e2e/functional/OrderTests.java
@@ -2,8 +2,11 @@ package giis.eshopcontainers.e2e.functional;
 
 import giis.eshopcontainers.e2e.functional.common.BaseLoggedClass;
 import giis.eshopcontainers.e2e.functional.common.ElementNotFoundException;
+import giis.eshopcontainers.e2e.functional.utils.Click;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import static giis.eshopcontainers.e2e.functional.utils.Shopping.addProductToBasket;
 
@@ -14,9 +17,13 @@ class OrderTests extends BaseLoggedClass {
         // Perform login
         this.login();
         // Add products to the basket
-        addProductToBasket(1, "NetCore Cup",driver,waiter);
-        addProductToBasket(3, "Hoodie",driver,waiter);
-        addProductToBasket(6, "Pin",driver,waiter);
+        addProductToBasket(2, "NetCore Cup",driver,waiter);
+        addProductToBasket(4, "Hoodie",driver,waiter);
+        addProductToBasket(5, "Pin",driver,waiter);
+
+
+        WebElement menuOrder=driver.findElement(By.xpath("/html/body/header/div/article/section[3]/a/div[2]"));
+        Click.element(driver,waiter,menuOrder);
 
         // Perform logout
         this.logout();
