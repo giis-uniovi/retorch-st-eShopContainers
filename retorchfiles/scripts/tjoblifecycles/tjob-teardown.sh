401dae3e16ac5af72e7f9d6840f0f854d6a3165f
Augusto <augustocristian@uniovi.es>
2023-12-09 23:04:20 +0100

    Improved in general selema,tests and containers logging
    Now each log is separated into a folder, that are stored at the end of the test execution in the Jenkins artifacts.

--- retorchfiles/scripts/tjoblifecycles/tjob-teardown.sh
+++ retorchfiles/scripts/tjoblifecycles/tjob-teardown.sh
@@ -8,6 +8,18 @@ execute_timestamp_script() {
 
 # Execute the script to write timestamp
 execute_timestamp_script "$1" "$2"
+# Store docker logs
+DIRECTORY_PATH="$WORKSPACE/target/containerlogs/$1"
+
+if [ ! -d "$DIRECTORY_PATH" ]; then
+  echo "Directory for storing logs doesnt exist creating..."
+  mkdir -p $DIRECTORY_PATH
+fi
+
+for CONTAINER_NAME in $(docker ps -a --format "{{.Names}}" --filter Name=$1); do
+  echo "Storing lof con container $CONTAINER_NAME in TJob $1"
+  docker logs $CONTAINER_NAME &>"$DIRECTORY_PATH/$CONTAINER_NAME.log"
+done
 
 # Change to SUT location
 cd "$SUT_LOCATION/src"
