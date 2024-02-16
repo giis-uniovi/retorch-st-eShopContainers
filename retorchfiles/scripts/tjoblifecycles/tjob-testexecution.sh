7ce3ad0aacd85399fba8fc1f69c87552892e6017
Augusto <augustocristian@uniovi.es>
2024-02-16 21:02:36 +0100

    Change to testFailureIngnore to false and changing script order to avoid bad time measurings

--- retorchfiles/scripts/tjoblifecycles/tjob-testexecution.sh
+++ retorchfiles/scripts/tjoblifecycles/tjob-testexecution.sh
@@ -38,15 +38,17 @@ LOCALHOST="$DOCKER_HOST_IP:$PORT"
 # Run Maven test
 mvn test -Ddirtarget="$TJOBNAME" -Dtest="$TEST_NAME" -Dtjob_name="$TJOB_NAME" -DSUT_URL="$SUT_URL" -DSUT_PORT="$PORT"
 
-# Check if Maven test failed
-if [ $? -ne 0 ]; then
-  echo "Maven test FAILED with exit code $?."
-  exit 1 # Return 1 if the Maven test fails
-fi
-
-# If the script reaches here, the Maven test succeeded
-echo "Maven test succeeded."
+# Store Build result
+MVN_EXIT_CODE=$?
 
 # Execute the script to write timestamp again
 EXECUTE_TIMESTAMP_SCRIPT "$STAGE" "$TJOB_NAME"
+# Check if Maven test failed
+if [ $MVN_EXIT_CODE -ne 0 ]; then
+  echo "Maven test FAILED with exit code $?."
+  exit 1 # Return 1 if the Maven test fails
+fi
+# If the script reaches here, the Maven test succeeded
+echo "Maven test succeeded."
+
 exit 0
