cbd8679fb0084898a6a317315c09b75c248c1f09
Augusto <augustocristian@uniovi.es>
2024-04-22 08:49:59 +0200

    Removing the function and adding logging system

--- retorchfiles/scripts/tjoblifecycles/tjob-setup.sh
+++ retorchfiles/scripts/tjoblifecycles/tjob-setup.sh
@@ -1,52 +1,58 @@
 #!/bin/bash
 set -e
+
+# Function to log messages
+log_message() {
+  echo "[$(date +"%Y-%m-%d %T")] [INFO] - $1"
+}
+
 # Execute the script to write timestamp
 "$SCRIPTS_FOLDER/writetime.sh" "$2" "$1"
 
 # Export Docker Host IP
 DOCKER_HOST_IP=$(/sbin/ip route | awk '/default/ { print $3 }')
 export DOCKER_HOST_IP
-echo "Exporting the HOST_IP: $DOCKER_HOST_IP"
+log_message "Exporting the HOST_IP: $DOCKER_HOST_IP"
 
 # Custom Set-up commands
-echo "Executing custom commands"
+log_message "Executing custom commands"
 
-copy_and_replace_envoy_configs() {
-  local tjobname="$1"
-  local CONFIG_DIR="$SUT_LOCATION/ApiGateways/Envoy/config"
-  local TMP_DIR="$SUT_LOCATION/tmp/$tjobname"
-  if [ -d "$TMP_DIR" ]; then
-    echo "Directory /tmp for the tjob $tjobname exist, removing...."
-    rm -r "$TMP_DIR"
-  else
-    echo "Directory /tmp for the tjob $tjobname does not exist."
-  fi
+tjobname="$1"
+CONFIG_DIR="$SUT_LOCATION/ApiGateways/Envoy/config"
+TMP_DIR="$SUT_LOCATION/tmp/$tjobname"
+if [ -d "$TMP_DIR" ]; then
+  log_message "Directory /tmp for the tjob $tjobname exist, removing...."
+  rm -r "$TMP_DIR"
+else
+  log_message "Directory /tmp for the tjob $tjobname does not exist."
+fi
 
-  mkdir -p "$TMP_DIR/mobileshopping"
-  mkdir -p "$TMP_DIR/webshopping"
+mkdir -p "$TMP_DIR/mobileshopping"
+mkdir -p "$TMP_DIR/webshopping"
 
-  cp -p "$CONFIG_DIR/mobileshopping/envoy.yaml" "$TMP_DIR/mobileshopping/"
-  cp -p "$CONFIG_DIR/webshopping/envoy.yaml" "$TMP_DIR/webshopping/"
+log_message "Copying the envoy files to the tmp directories for tjob $1"
 
-  sed -i "s/\${tjobname}/$tjobname/g" "$TMP_DIR/mobileshopping/envoy.yaml"
-  sed -i "s/\${tjobname}/$tjobname/g" "$TMP_DIR/webshopping/envoy.yaml"
+cp -p "$CONFIG_DIR/mobileshopping/envoy.yaml" "$TMP_DIR/mobileshopping/"
+cp -p "$CONFIG_DIR/webshopping/envoy.yaml" "$TMP_DIR/webshopping/"
 
-  chmod go+r "$TMP_DIR/mobileshopping/envoy.yaml"
-  chmod go+r "$TMP_DIR/webshopping/envoy.yaml"
-}
+log_message "Reemplacing the TJob name in the envoy conf files"
+sed -i "s/\${tjobname}/$tjobname/g" "$TMP_DIR/mobileshopping/envoy.yaml"
+sed -i "s/\${tjobname}/$tjobname/g" "$TMP_DIR/webshopping/envoy.yaml"
 
-copy_and_replace_envoy_configs "$1"
+log_message "Changing the permissions for tjob $1"
+chmod go+r "$TMP_DIR/mobileshopping/envoy.yaml"
+chmod go+r "$TMP_DIR/webshopping/envoy.yaml"
 
 # Deploy containers
 cd "$SUT_LOCATION"
-echo "Deploying containers for TJOB $1"
+log_message "Deploying containers for TJOB $1"
 docker compose -f docker-compose.yml --env-file "$WORKSPACE/retorchfiles/envfiles/$1.env" --ansi never -p "$1" up -d
 
-echo "Waiting for the system to be up..."
+log_message "Waiting for the system to be up..."
 "$WORKSPACE/retorchfiles/scripts/waitforSUT.sh" "$1"
 cd "$WORKSPACE"
 
-echo "System READY!! Test execution can start!"
+log_message "System READY!! Test execution can start!"
 
 # Execute the script to write timestamp again
 "$SCRIPTS_FOLDER/writetime.sh" "$2" "$1"
