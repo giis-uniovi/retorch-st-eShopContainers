ba175bdb4497c66137ec88d6319ad20168a9d473
Augusto <augustocristian@uniovi.es>
2024-05-10 11:27:04 +0200

    Undo to prior set-up

--- retorchfiles/scripts/tjoblifecycles/tjob-setup.sh
+++ retorchfiles/scripts/tjoblifecycles/tjob-setup.sh
@@ -11,46 +11,35 @@ export DOCKER_HOST_IP
 # Custom Set-up commands
 "$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "$1-set-up" "Executing custom commands"
 
-# Custom Set-up commands
-"$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "$1-set-up" "Executing custom commands"
+copy_and_replace_envoy_configs() {
+    local tjobname="$1"
+    mkdir -p "$SUT_LOCATION/tmp/$tjobname/mobileshopping/"
+    mkdir -p "$SUT_LOCATION/tmp/$tjobname/webshopping/"
 
-tjobname="$1"
-CONFIG_DIR="./ApiGateways/Envoy/config"
-TMP_DIR="./tmp/$1"
+    "$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "$1-set-up" "Copying the envoy files to the tmp directories for tjob $1"
+    cp -p "$SUT_LOCATION/ApiGateways/Envoy/config/mobileshopping/envoy.yaml" "$SUT_LOCATION/tmp/$tjobname/mobileshopping/"
+    cp -p "$SUT_LOCATION/ApiGateways/Envoy/config/webshopping/envoy.yaml" "$SUT_LOCATION/tmp/$tjobname/webshopping/"
 
-mkdir -p "$TMP_DIR/mobileshopping"
-mkdir -p "$TMP_DIR/webshopping"
+    "$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "$1-set-up" "Reemplacing the TJob name in the envoy conf files"
+    sed -i "s/\${tjobname}/$tjobname/g" "$SUT_LOCATION/tmp/$tjobname/mobileshopping/envoy.yaml"
+    sed -i "s/\${tjobname}/$tjobname/g" "$SUT_LOCATION/tmp/$tjobname/webshopping/envoy.yaml"
+}
 
-"$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "$1-set-up" "Copying the envoy files to the tmp directories for tjob $1"
-
-cp -p "$CONFIG_DIR/mobileshopping/envoy.yaml" "$TMP_DIR/mobileshopping/"
-cp -p "$CONFIG_DIR/webshopping/envoy.yaml" "$TMP_DIR/webshopping/"
-
-"$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "$1-set-up" "Reemplacing the TJob name in the envoy conf files"
-sed -i "s/\${tjobname}/$1/g" "$TMP_DIR/mobileshopping/envoy.yaml"
-sed -i "s/\${tjobname}/$1/g" "$TMP_DIR/webshopping/envoy.yaml"
-
-"$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "$1-set-up" "Changing the permissions for tjob $1"
-chmod go+r "$TMP_DIR/mobileshopping/envoy.yaml"
-chmod go+r "$TMP_DIR/webshopping/envoy.yaml"
-
+copy_and_replace_envoy_configs "$1"
 
 "$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "$1-set-up" "End custom commands"
 
-"$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "$1-set-up" "Some sleep prior the deployment"
-
-ls -la
 # Deploy containers
 cd "$SUT_LOCATION"
 
-ls -la
-
 "$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "$1-set-up" "Deploying containers for TJOB $1"
+
 docker compose -f docker-compose.yml --env-file "$WORKSPACE/retorchfiles/envfiles/$1.env" --ansi never -p "$1" up -d
 
 "$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "$1-set-up" "Waiting for the system to be up..."
+
 "$WORKSPACE/retorchfiles/scripts/waitforSUT.sh" "$1"
-cd "$WORKSPACE"
+
 
 "$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "$1-set-up" "System READY!! Test execution can start!"
 
