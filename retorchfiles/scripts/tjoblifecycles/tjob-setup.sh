4b529021aceb5f7f18fd1b6415cb34caa8b98566
Augusto <augustocristian@uniovi.es>
2024-05-10 15:06:50 +0200

    Removing sed from set-up moving custom commands

--- retorchfiles/scripts/tjoblifecycles/tjob-setup.sh
+++ retorchfiles/scripts/tjoblifecycles/tjob-setup.sh
@@ -11,32 +11,6 @@ export DOCKER_HOST_IP
 # Custom Set-up commands
 "$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "$1-set-up" "Executing custom commands"
 
-copy_and_replace_envoy_configs() {
-    local TJOB_NAME="$1"
-    echo $TJOB_NAME
-    mkdir -p "$SUT_LOCATION/tmp/$TJOB_NAME/mobileshopping"
-    mkdir -p "$SUT_LOCATION/tmp/$TJOB_NAME/webshopping"
-
-    "$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "$1-set-up" "Copying the envoy files to the tmp directories for tjob $1"
-    cp -p "$SUT_LOCATION/ApiGateways/Envoy/config/mobileshopping/envoy.yaml" "$SUT_LOCATION/tmp/$TJOB_NAME/mobileshopping/"
-    cp -p "$SUT_LOCATION/ApiGateways/Envoy/config/webshopping/envoy.yaml" "$SUT_LOCATION/tmp/$TJOB_NAME/webshopping/"
-
-    "$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "$1-set-up" "Reemplacing the TJob name in the envoy conf files"
-    sed -i "s/\${tjobname}/$TJOB_NAME/g" "$SUT_LOCATION/tmp/$TJOB_NAME/mobileshopping/envoy.yaml"
-    sed -i "s/\${tjobname}/$TJOB_NAME/g" "$SUT_LOCATION/tmp/$TJOB_NAME/webshopping/envoy.yaml"
-
-    "$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "$1-set-up" "Change envoy file permisions"
-    ls -la "$SUT_LOCATION/tmp/$TJOB_NAME/mobileshopping"
-    ls -la "$SUT_LOCATION/tmp/$TJOB_NAME/webshopping"
-    chmod 777 "$SUT_LOCATION/tmp/$TJOB_NAME/mobileshopping/envoy.yaml"
-    chmod 777 "$SUT_LOCATION/tmp/$TJOB_NAME/webshopping/envoy.yaml"
-    ls -la "$SUT_LOCATION/tmp/$TJOB_NAME/mobileshopping"
-    ls -la "$SUT_LOCATION/tmp/$TJOB_NAME/webshopping"
-}
-
-copy_and_replace_envoy_configs "$1"
-
-"$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "$1-set-up" "End custom commands"
 
 # Deploy containers
 cd "$SUT_LOCATION"
@@ -44,6 +18,8 @@ cd "$SUT_LOCATION"
 "$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "$1-set-up" "Building proxy images for TJOB $1"
 
 docker compose -f "docker-compose.yml" --env-file "$WORKSPACE/retorchfiles/envfiles/$1.env" --ansi never build webshoppingapigw mobileshoppingapigw
+
+"$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "$1-set-up" "End custom commands"
 
 "$SCRIPTS_FOLDER/printLog.sh" "DEBUG" "$1-set-up" "Deploying containers for TJOB $1"
 
